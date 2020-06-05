aa219288e38b68bb65b3bb1679076807fe1d09aa
Daniel Joseph Antrim <daniel.joseph.antrim@cern.ch>
2020-06-05 16:07:56 -0700

    Site updated at 2020-06-05 23:07:56 UTC

---
+++ downloads/code/hello.py
@@ -0,0 +1,6 @@
+
+def coolio() :
+    # this is embedded
+    print("butts")
+if __name__ == "__coolio__" :
+    coolio()
---
+++ downloads/code/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // this code has been embedded from the file test.cxx!
+    cout << "Hello from embedded code!" << endl;
+    cout << "cpp" << endl;
+    return 0;
+}
